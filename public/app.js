11ed1762aad44eed5262ca5571a347dad854a815
Spin Wang <spinwang525@gmail.com>
2016-09-25 11:15:50 -0400

    convert url to hyperlink

--- public/app.js
+++ public/app.js
@@ -3,7 +3,8 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
-  'smsdruid.numbers',
+  'ngSanitize',
+  'smsdruid.numbers'
 
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
--- public/view/number.js
+++ public/view/number.js
@@ -15,15 +15,15 @@ angular.module('smsdruid.numbers', ['ngRoute'])
       self.numbers = {};
       self.msgs = {};
 
-      // Initialize Firebase
-      // todo: hid the credential
+
+      // todo: hide the credential
       var config = {
         apiKey: "AIzaSyDVAzFNruUePIDKWACy5Acbsok4Cklrx9A",
         authDomain: "smsdruid-590fa.firebaseapp.com",
         databaseURL: "https://smsdruid-590fa.firebaseio.com",
         storageBucket: "smsdruid-590fa.appspot.com"
       };
-      firebase.initializeApp(config);
+      firebase.initializeApp(config); // Initialize Firebase
       var database = firebase.database();
       var numbersRef = database.ref('/numbers');
       var msgsRef = database.ref('/msgs').limitToFirst(20);
@@ -38,6 +38,11 @@ angular.module('smsdruid.numbers', ['ngRoute'])
       msgsRef.on('child_added',function(snapshot){
         var value = snapshot.val();
         var key = snapshot.getKey();
+        var msg_body = value.Body;
+        if (msg_body) {
+          msg_body = msg_body.autoLink(); // convert the url to html hyperlinks
+          value.Body = msg_body;
+        }
         self.msgs[key] = value;
         $scope.$apply();
       })
